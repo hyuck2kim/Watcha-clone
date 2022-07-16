e1a886edc8ad48ce7678f414a8ae5e8125bd4c5a
unknown <sang_zz@naver.com>
2022-07-16 18:37:22 +0900

    first push for changing labtop

---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from '../Components/Footer';
+import Header from '../Components/Header'
+
+const MainPage: React.FC = () => {
+    return (
+        <div><Header /><Footer /></div>
+    )
+}
+
+export default MainPage;
---
+++ src/Pages/TvDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TVDetail: React.FC = () =>{
+    return(
+        <div>TVDetail</div>
+    )
+}
+
+export default TVDetail;
